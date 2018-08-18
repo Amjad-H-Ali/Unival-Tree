49d4fba9b502a00b28b6cb06b4af0520563ada3f
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-17 19:30:33 -0500

    Created Tree Class and instantiated root object

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 console.log('Unival Tree!');
+
+class Tree {
+
+	constructor (value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+		
+	}
+}
+
+
+
+const root = new Tree (5);
+
+
+
+
