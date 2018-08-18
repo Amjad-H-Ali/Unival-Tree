025a850ed0ea2dda05e9d257c1d9bf463e87ff13
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-17 19:53:26 -0500

    Created a Tree Stucture with expected output of 4

--- js/index.js
+++ js/index.js
@@ -4,16 +4,51 @@ class Tree {
 
 	constructor (value) {
 		this.value = value;
-		this.left = null;
-		this.right = null;
+		this.left;
+		this.right;
 		
 	}
 }
 
+/*
+
+	Node:
+			 5
+		   /   \
+		  /     \
+		 5	     5
+	   /   \    /    
+	  5     5  1   
+
+	
+	Output: 4
+
+
+*/	  
 
 
 const root = new Tree (5);
 
+root.left = new Tree (5);
 
+root.right = new Tree (5);
+
+root.left.left = new Tree (5);
+
+root.left.right = new Tree (5);
+
+root.right.left = new Tree (1);
+
+console.log(root.value);
+
+console.log(root.left.value);
+
+console.log(root.right.value);
+
+console.log(root.left.left.value);
+
+console.log(root.left.right.value);
+
+console.log(root.right.left.value);
 
 
